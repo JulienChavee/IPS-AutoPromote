1e7b200a85924a82d2909c9c74a0492dc34822c6
Julien Chavée <julien.chavee@gmail.com>
2017-11-05 09:39:24 +0100

    Version 1.0.6

--- p38b2f225e3/dev/lang.php
+++ p38b2f225e3/dev/lang.php
@@ -1,7 +1,9 @@
 <?php
 
 $lang = array(
-
-
-
+	'promote_clubs' => 'Clubs dont les membres doivent être promus',
+	'not_delete_members_in_club' => 'Clubs pour lequels il ne faut pas retirer les membres',
+	'group_non_membre' => 'Groupe dans lequel seront transférés les membres n\'appartenant plus à Fail',
+	'group_conseil_alliance' => 'Groupe des membres du conseil d\'alliance',
+	'group_alliance' => 'Groupe des membres de l\'alliance'
 );
--- p38b2f225e3/settings.php
+++ p38b2f225e3/settings.php
@@ -1,15 +1,33 @@
 //<?php
 
 $clubs = json_decode( \IPS\Settings::i()->promote_clubs );
+$not_delete_members_in_club = json_decode( \IPS\Settings::i()->not_delete_members_in_club );
 $group_alliance = \IPS\Settings::i()->group_alliance;
 $group_conseil_alliance = \IPS\Settings::i()->group_conseil_alliance;
 $group_non_membre = \IPS\Settings::i()->group_non_membre;
 
-//$form->add( new \IPS\Helpers\Form\Text( 'promote_clubs', '', TRUE, array( 'autocomplete' => array( 'source' => 'app=avacalendar&module=calendar&controller=manage&do=getZone', 'maxItems' => 1, 'prefix' => FALSE ), 'placeholder' => 'avacalendar_calendar_manage_zone' ) ) );
+$all_clubs = \IPS\Db::i()->select( '*', 'core_clubs' );
+
+$optionsClub = array();
+
+foreach( $all_clubs as $k => $v )
+{
+	$optionsClub[ $v[ 'id' ] ] = $v[ 'name' ];
+}
+
+$form->add( new \IPS\Helpers\Form\Select( 'promote_clubs', $clubs, FALSE, array( 'options' => $optionsClub, 'sort' => TRUE, 'multiple' => true, 'parse' => 'normal' ) ) );
+$form->add( new \IPS\Helpers\Form\Select( 'not_delete_members_in_club', $not_delete_members_in_club, FALSE, array( 'options' => $optionsClub, 'sort' => TRUE, 'multiple' => true, 'parse' => 'normal' ) ) );
+$form->add( new \IPS\Helpers\Form\Select( 'group_alliance', $group_alliance, TRUE, array( 'options' => \IPS\Member\Group::groups(), 'sort' => TRUE, 'parse' => 'normal' ) ) );
+$form->add( new \IPS\Helpers\Form\Select( 'group_conseil_alliance', $group_conseil_alliance, TRUE, array( 'options' => \IPS\Member\Group::groups(), 'sort' => TRUE, 'parse' => 'normal' ) ) );
+$form->add( new \IPS\Helpers\Form\Select( 'group_non_membre', $group_non_membre, TRUE, array( 'options' => \IPS\Member\Group::groups(), 'sort' => TRUE, 'parse' => 'normal' ) ) );
+
 
 if ( $values = $form->values() )
 {
-	$form->saveAsSettings();
+	$values['promote_clubs'] = json_encode( array_values( $values['promote_clubs'] ) );
+	$values['not_delete_members_in_club'] = json_encode( array_values( $values['not_delete_members_in_club'] ) );
+
+	$form->saveAsSettings( $values );
 	return TRUE;
 }
 
